8cb5d565883ae20c1fefbc1d0a8473f1d12f03b4
Johnny <jcarreiropublicidade@gmail.com>
2021-04-19 16:08:26 -0300

    feat: Add accordion e detailed texts about projects

--- src/components/sections/ProjectsSection/index.tsx
+++ src/components/sections/ProjectsSection/index.tsx
@@ -1,4 +1,4 @@
-import Button from 'components/Button'
+import Accordion from 'components/Accordion'
 import { Container } from './styles'
 
 
@@ -18,13 +18,14 @@ export const ProjectsSection:React.FC = () => {
           <p>
             Precisa começar ou formalizar sua empresa, este projeto existe para que você possa contar com soluções completas para o desenvolvimento e crescimento da sua maca.
           </p>
-          <Button
-            isPrimary={false}
-            primaryColor={false}
-            text="Saiba mais"
-            link="#"
-            type="button"
-          />
+          <Accordion title="Saiba mais" >
+            <h3>Desenvolvimento da sua marca</h3>
+            <p>
+              Antes de começar ou formalizar qualquer empresa é necessário se planejar e desenvolver sua marca. Isso vai além de uma logo bonita e um nome legal , sua marca é coisa mais importante da sua empresa, será a primeira coisa a ser vista pelo seu cliente e ela tem que ser impactante e transmitir de forma eficiente e clara o que sua empresa representa.<br/>
+              Mas só uma marca bem feita não garante o seu sucesso, planejamento é essencial. Para isso iremos te guiar em como você pode montar um planejamento estratégico e um plano de negócio, que com certeza será um diferencial na sua escalada para o sucesso.<br/>
+              Tenha certeza que você vai começar seu empreendimento com uma mapa e uma bússola.
+            </p>
+          </Accordion>
         </div>
       </div>
 
@@ -37,51 +38,58 @@ export const ProjectsSection:React.FC = () => {
           <p>
             Esses projetos destinam-se a aqueles clientes que desejam possuir sua própria plataforma, seus dados e veem a necessidade no futuro de poder personalizar toda sua experiência de oferta de produtos e serviços.
           </p>
-          <Button
-            isPrimary={false}
-            primaryColor={false}
-            text="Saiba mais"
-            link="#"
-            type="button"
-          />
+          <Accordion title="Saiba mais" >
+            <h3>Sites e materias para marketing</h3>
+            <p>
+              Este projeto é para clientes que desejam possuir suas plataformas e seus dados e veem a necessidade de no futuro desenvolver toda a experiência de oferta de produtos e serviços de forma customizada.<br/>
+              Para este projeto, partimos do site,  materiais de apoio, como papelarias, modelos de e-mail marketing e templates para marketing em redes sociais, configurações de contas de e-mail e consultorias para que possa fazer a gestão de suas próprias campanhas publicitárias.<br/>
+              Esse tipo de projeto fica evidente que você tem um produto básico bem especificado, de sua propriedade e por um preço fixo, enquanto pode fazer as alterações de que precisa. e você vai começar seu empreendimento com uma mapa e uma bússola.
+            </p>
+          </Accordion>
         </div>
       </div>
 
-      <div className="container3">
-        <div className="imageContainer3" >
+      <div className="container1">
+        <div className="imageContainer1" >
           <img src="/images/medium.svg" alt="Back in Business"/>
         </div>
-        <div className="mainContent3">
+        <div className="mainContent1">
           <h2>Médios projetos</h2>
           <p>
             À medida que as vendas aumentam e o pessoal aumenta, surge uma maior necessidade de automação. Os detalhes se tornam mais importantes à medida que o número de visitantes aumenta, e pequenas coisas podem fazer uma grande diferença.
           </p>
-          <Button
-            isPrimary={false}
-            primaryColor={false}
-            text="Saiba mais"
-            link="#"
-            type="button"
-          />
+          <Accordion title="Saiba mais" >
+            <h3>E-commerce,  marketing e Soluções mais complexas</h3>
+            <p>
+              Durante o crescimento e desenvolvimento de um website, surge uma maior necessidade de automação, possíveis integrações com diferentes sistemas e coloca uma pressão mais significativa nas operações diárias. Os detalhes tornam-se cada vez mais críticos à medida que aumenta o número de visitantes.<br/>
+              Os projetos são uma colaboração estreita entre nós, onde usarei um gerenciamento inteligente de projetos para poder tomar decisões mais eficientes sobre o consumo de tempo e a priorização durante o projeto. Aliados a treinamentos para suas equipes, como sistemas novos integrados, lapidação das técnicas de vendas e implementação e acompanhamento de processo internos e fluxo de informações entre diferentes setores.<br/>
+              No âmbito do marketing digital, definiremos juntamente o budget a ser investido e assumiremos a produção de conteúdo dedicados, planejamento e execução das  campanhas.<br/>
+              Também é de costume ter uma quantidade de tempo planejada a cada mês, um banco de horas, não cumulativo, que usamos para fazer melhorias contínuas dentro da estrutura de nosso desenvolvimento posterior  ao lançamento do projeto, produção de conteúdos extras, solução de problemas e situações inesperadas que possam surgir no decorrer de nossa prestação de serviços.
+            </p>
+          </Accordion>
         </div>
       </div>
 
-      <div className="container4">
-        <div className="imageContainer4" >
+      <div className="container2">
+        <div className="imageContainer2" >
           <img src="/images/large.svg" alt="Back in Business"/>
         </div>
-        <div className="mainContent4">
+        <div className="mainContent2">
           <h2>Grandes projetos</h2>
           <p>
             Quando uma empresa está aqui, percebe que, para obter o melhor resultado possível para o seu negócio, a equipe precisa das melhores ferramentas possíveis e essas ferramentas muitas vezes precisam ser personalizadas.
           </p>
-          <Button
-            isPrimary={false}
-            primaryColor={false}
-            text="Saiba mais"
-            link="#"
-            type="button"
-          />
+          <Accordion title="Saiba mais" >
+            <h3>Desenvolvimento de Aplicações para transformação digital e automação</h3>
+            <p>
+              As pessoas não devem trabalhar com tarefas repetitivas e administrativas, mas, em vez disso, deveriam ter a permissão de usar sua humanidade e criatividade para criar laços fortes com outras pessoas, clientes, fornecedores e fazer coisas que são difíceis de automatizar. Pode ser difícil sem ferramentas feitas sob medida, projetadas com uma boa experiência do usuário.<br/>
+              Somente quando essas ferramentas personalizadas estão disponíveis é que o potencial do negócio pode realmente se concretizar e a produtividade geralmente aumenta significativamente.<br/>
+              Nestes projetos, trabalhamos, principalmente, com o framework e tecnologias para desenvolvimento de sites e aplicativos multiplataforma. Geralmente, eles são integrados ao sistemas já existentes em sua empresa, mas podem funcionam como aplicativos totalmente independentes.<br/>
+              Esses projetos são grandes e complexos por natureza e têm várias dependências complicadas. Portanto, essas colaborações são contínuas e focadas na corrida e na solução de problemas complicados de negócios, juntos.<br/>
+              Ou seja, este projeto destina-se a desenvolvimento e integração continua, para que possamos ter sistemas sempre atualizados, mais seguros e performáticos, proporcionando uma entrega melhor de nossas soluções, visando sempre a marca e a experiência dos usuários.<br/>
+              É comum termos um período planejado de tempo a cada mês para fazer reuniões e melhorias contínuas dentro da estrutura de nosso desenvolvimento, posterior ao lançamento do projeto, produção de conteúdos extras, solução de problemas e situações inesperadas que possam surgir no decorrer de nossa prestação de serviços.
+            </p>
+          </Accordion>
         </div>
       </div>
     </Container>
--- src/components/sections/ProjectsSection/styles.ts
+++ src/components/sections/ProjectsSection/styles.ts
@@ -26,7 +26,6 @@ export const Container = styled.div`
         flex:1;
         width:100%;
         height:100%;
-        padding-right: 2rem;
         img{
           grid-area:image1;
         }
@@ -35,6 +34,11 @@ export const Container = styled.div`
         order:1;
         flex:2;
         color: ${({theme})=>theme.color.gray_200};
+        padding-right: 2rem;
+
+        > p {
+          margin-bottom:2rem;
+        }
 
         div{
           & + div{
@@ -64,61 +68,9 @@ export const Container = styled.div`
         flex:2;
         color: ${({theme})=>theme.color.gray_200};
 
-        div{
-          & + div{
-            margin-top: 2rem;
-          }
+        > p {
+          margin-bottom:2rem;
         }
-      }
-    }
-  //container3
-  .container3{
-    display:flex;
-    align-items:flex-start;
-    margin:2rem auto;
-    max-width:1140px;
-      .imageContainer3{
-        order:2;
-        flex:1;
-        width:100%;
-        height:100%;
-        padding-right: 2rem;
-        img{
-          grid-area:image1;
-        }
-      }
-      .mainContent3{
-        order:1;
-        flex:2;
-        color: ${({theme})=>theme.color.gray_200};
-
-        div{
-          & + div{
-            margin-top: 2rem;
-          }
-        }
-      }
-  }
-  //container4
-  .container4{
-    display:flex;
-    align-items:flex-start;
-    margin:2rem auto;
-    max-width:1140px;
-      .imageContainer4{
-        order:1;
-        flex:1;
-        width:100%;
-        height:100%;
-        padding-right: 2rem;
-        img{
-          grid-area:image1;
-        }
-      }
-      .mainContent4{
-        order:2;
-        flex:2;
-        color: ${({theme})=>theme.color.gray_200};
 
         div{
           & + div{
@@ -128,96 +80,50 @@ export const Container = styled.div`
       }
     }
 
-  @media screen and (max-width:1024px){
-    .projects{
-      margin:3rem 2rem;
-    }
-    .container1{
-      display:flex;
-      flex-direction:column;
-      align-items:flex-start;
-      margin:0 2rem;
-
-      & + div{
-        margin-top: 4rem;
+    @media screen and (max-width:1024px) {
+      .projects{
+        margin:3rem 2rem;
       }
-      .imageContainer1{
-        order:1;
-        margin-bottom: 2rem;
-      }
-      .mainContent1{
-        color: ${({theme})=>theme.color.gray_200};
+      .container1{
         display:flex;
         flex-direction:column;
-        align-items:center;
-        text-align:start;
+        align-items:flex-start;
+        margin:0 2rem;
+        & + div{
+          margin-top: 4rem;
+        }
+        .imageContainer1{
+          order:1;
+          margin-bottom: 2rem;
+        }
+        .mainContent1{
+          color: ${({theme})=>theme.color.gray_200};
+          display:flex;
+          flex-direction:column;
+          align-items:center;
+          text-align:start;
+        }
       }
-    }
-    //container 2
-    .container2{
-      display:flex;
-      flex-direction:column;
-      align-items:flex-start;
-      margin:0 2rem;
-
-      & + div{
-        margin-top: 4rem;
-      }
-      .imageContainer2{
-        order:1;
-        margin-bottom: 2rem;
-      }
-      .mainContent2{
-        color: ${({theme})=>theme.color.gray_200};
+      //container 2
+      .container2{
         display:flex;
         flex-direction:column;
-        align-items:center;
-        text-align:start;
+        align-items:flex-start;
+        margin:0 2rem;
+        & + div{
+          margin-top: 4rem;
+        }
+        .imageContainer2{
+          order:1;
+          margin-bottom: 2rem;
+        }
+        .mainContent2{
+          color: ${({theme})=>theme.color.gray_200};
+          display:flex;
+          flex-direction:column;
+          align-items:center;
+          text-align:start;
+        }
       }
     }
-    //container 3
-    .container3{
-      display:flex;
-      flex-direction:column;
-      align-items:flex-start;
-      margin:0 2rem;
-
-      & + div{
-        margin-top: 4rem;
-      }
-      .imageContainer3{
-        order:1;
-        margin-bottom: 2rem;
-      }
-      .mainContent3{
-        color: ${({theme})=>theme.color.gray_200};
-        display:flex;
-        flex-direction:column;
-        align-items:center;
-        text-align:start;
-      }
-    }
-    //container 4
-    .container4{
-      display:flex;
-      flex-direction:column;
-      align-items:flex-start;
-      margin:0 2rem;
-
-      & + div{
-        margin-top: 4rem;
-      }
-      .imageContainer4{
-        order:1;
-        margin-bottom: 2rem;
-      }
-      .mainContent4{
-        color: ${({theme})=>theme.color.gray_200};
-        display:flex;
-        flex-direction:column;
-        align-items:center;
-        text-align:start;
-      }
-    }
-  }
 `
