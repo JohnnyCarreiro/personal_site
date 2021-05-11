9b4c7f4b872b0b6bf5af36d9bce988e5d596be2c
Johnny <jcarreiropublicidade@gmail.com>
2021-05-11 18:50:10 -0300

    fix: Fix animation to start when comp is on screen

--- src/components/sections/RequestAMeetingSection/index.tsx
+++ src/components/sections/RequestAMeetingSection/index.tsx
@@ -1,14 +1,19 @@
 import { AnimatedImg } from 'components/AnimatedImg'
 import { RequestForm } from 'components/RequestForm'
 import { a, useSpring } from '@react-spring/web'
+
+import { useVisibility } from 'utils/useVisibility'
 import { Container } from './styles'
 
 
 export const RequestAMeetingSection:React.FC = () => {
+
+  const [isVisible, currentElement] = useVisibility<HTMLDivElement>(100)
+
   const animation = useSpring({
     from: { opacity: 0, scale: 0, config:{ easing:true } },
     to: async (next, _) => {
-      await next({ opacity: 1, scale:1, delay: 600 })
+      isVisible && await next({ opacity: 1, scale:1, delay: 600 })
     }
   })
   return (
@@ -19,7 +24,7 @@ export const RequestAMeetingSection:React.FC = () => {
             Pronto para iniciar um novo projeto conosco? Excelente! Ligue para nós ou nos envie uma e-mail e vamos retornar assim que possível!
           </p>
       </div>
-      <a.div className="container" style={animation} >
+      <a.div ref={currentElement} className="container" style={animation} >
         <AnimatedImg
           className="imageContainer"
           image="/images/request_meeting.svg"
